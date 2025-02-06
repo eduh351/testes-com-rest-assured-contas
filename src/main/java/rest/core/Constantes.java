a3a78a109c0ef68fa8419baffc5463fc5e1a6950
DESKTOP-DOMD9ND\duard <eduardosilva@apoioprodesp.sp.gov.br>
2025-02-06 20:03:26 -0300

    segundo commit

---
+++ src/main/java/rest/core/Constantes.java
@@ -0,0 +1,17 @@
+package rest.core;
+
+import io.restassured.http.ContentType;
+
+public interface Constantes {
+	
+	// Url  base dos testes 
+	String BASE_URL = "http://barrigarest.wcaquino.me";
+	
+	// tipo de requisição que a API trabalha 
+	ContentType CONTENT_TYPE = ContentType.JSON;
+	
+	// Tempo máximo  de resposta para cada requisição 
+	long MAX_TIMEOUT = 5000; 
+	
+
+}
